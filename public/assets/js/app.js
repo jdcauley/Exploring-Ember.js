3ab1b4c9660323e658b862388d8fab8fbf830793
Jordan Cauley <jordan@cauley.co>
2014-02-24 16:18:48 -0500

    New files and updated files to 3.2

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,6 +2,46 @@ App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
 
+App.PRODUCTS = [
+  {
+    title: 'Flint',
+    price: 99,
+    description: 'Flint is awesome',
+    isOnSale: true,
+    image: 'assets/images/logo.jpg'
+  },
+  {
+    title: 'Kindling',
+    price: 249,
+    description: 'Easily start fires',
+    isOnSale: false,
+    image: 'assets/images/logo.jpg'
+  }
+];
+
 App.Router.map(function(){
   this.route('about');
+  this.resource('products');
 });
+
+App.IndexController = Ember.Controller.extend({
+  productsCount: 6,
+  logo: 'assets/images/logo.jpg',
+  time: function(){
+    return(new Date()).toDateString()
+  }.property()
+});
+
+App.AboutController = Ember.Controller.extend({
+  contactName: "Jordan",
+  avatar: 'assets/images/logo.jpg',
+  time: function(){
+    return((new Date()).getDay() === 0) ? "Closed" : "Open"; 
+  }.property()
+});
+
+App.ProductsRoute = Ember.Route.extend({
+  model: function() {
+    return App.PRODUCTS;
+  }
+})
